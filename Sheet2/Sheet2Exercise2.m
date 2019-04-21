62a0b70752ba407fb03932f83cf0377e06136a82
Leif Van Holland <holland@uni-bonn.de>
2019-04-21 21:17:12 +0200

    improve plots for exc 2

--- Sheet2/Sheet2Exercise2.m
+++ Sheet2/Sheet2Exercise2.m
@@ -6,7 +6,11 @@ N = 512;
 t = 0:1/N:1;
 f = cos(2*pi*4*t) + 4*cos(2*pi*10*t) + 6*cos(2*pi*18*t);
 
+subplot(2,1,1)
 stem(t,f)
+title("f (time domain)")
+ylabel("amplitude")
+xlabel("time [sec]")
 
 fhat = 0*t;
 fhat(4) = 0.5;
@@ -16,4 +20,8 @@ fhat(N-10) = 2;
 fhat(18) = 3;
 fhat(N-18) = 3;
 
+subplot(2,1,2)
 stem(t,fhat)
+title("f (frequency domain)")
+ylabel("magnitude")
+xlabel("frequency")
